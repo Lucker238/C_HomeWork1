009771988610e4f460f753e022bb506f450ce7fb
Maksim <md.sinikov@gmail.com>
2022-08-05 14:37:25 +0500

    Решили задачу 2

---
+++ Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+int min = 0;
+if(a > b)
+{
+    max = a;
+    min = b;
+}
+else
+{
+    max = b;
+    min = a;
+}
+Console.WriteLine($"{max} больше чем {min}");
