7d2a12b45abb25aaaa7ee637920ac1276b984233
Maksim <md.sinikov@gmail.com>
2022-08-05 14:53:52 +0500

    Решили задачу 4

---
+++ Task4/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if(a > b)
+{
+    max = a;
+}
+else
+{
+    max = b;
+}
+if(c > max)
+{
+    max = c;
+}
+Console.WriteLine($"Из чисел {a}, {b} и {c} самое большое число {max}.");
