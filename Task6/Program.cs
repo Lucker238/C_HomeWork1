b2c32447d91486b3b3a8610b8e66ea533356198e
Maksim <md.sinikov@gmail.com>
2022-08-05 15:01:43 +0500

    Сделал задачу 6

---
+++ Task6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int value = Convert.ToInt32(Console.ReadLine());
+if(value % 2 == 0)
+{
+    Console.WriteLine($"Число {value} является чётным.");
+}
+else
+{
+    Console.WriteLine($"Число {value} является нечётным.");
+}
